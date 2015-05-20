9c64dffdd529ee7f58959a4d651e98e329346985
Alexander Matskevich <sasha_minskbel@mail.ru>
2015-05-20 15:53:33 +0300

    util files

---
+++ src/main/java/bsu/fpmi/chat/util/MessageUtil.java
@@ -0,0 +1,41 @@
+package bsu.fpmi.chat.util;
+
+
+import bsu.fpmi.chat.model.Message;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class MessageUtil {
+
+    public static  final String TOKEN = "token";
+    public static final String MESSAGES = "messages";
+    private static final String TN = "TN";
+    private static final String EN = "EN";
+    private static final String USER = "user";
+    private static final String MESSAGE = "message";
+
+    public static String getToken(int index) {
+        Integer number = index * 8 + 11;
+        return TN + number + EN;
+    }
+
+    public static int getIndex(String token) {
+        return (Integer.valueOf(token.substring(2, token.length() - 2)) - 11) / 8;
+    }
+
+    public static JSONObject fromStringToJSON(String data) throws ParseException {
+        JSONParser parser = new JSONParser();
+        return (JSONObject) parser.parse(data.trim());
+    }
+
+    public static Message fromJsonToMessage(JSONObject json){
+        String user = (String) json.get(USER);
+        String messageText = (String) json.get(MESSAGE);
+        if (user != null && messageText != null){
+            return new Message(messageText, user);
+        }
+        return null;
+    }
+
+}
