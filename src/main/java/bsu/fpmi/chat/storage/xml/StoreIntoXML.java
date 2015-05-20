fb801990ff59d30c69f1acce991ced326ac851ec
Alexander Matskevich <sasha_minskbel@mail.ru>
2015-05-20 15:50:03 +0300

    XML storage realized

---
+++ src/main/java/bsu/fpmi/chat/storage/xml/StoreIntoXML.java
@@ -0,0 +1,169 @@
+package bsu.fpmi.chat.storage.xml;
+
+import bsu.fpmi.chat.model.Message;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class StoreIntoXML {
+
+    private static final String STORAGE_LOCATION = System.getProperty("user.home") +  File.separator + "history.xml";
+    private static final String MESSAGES = "messages";
+    private static final String MESSAGE = "message";
+    private static final String USER = "user";
+    private static final String MESSAGETEXT = "messageText";
+    private static final String ID = "id";
+
+    private StoreIntoXML() {}
+
+    public static synchronized void createStorage() throws ParserConfigurationException, TransformerException {
+        System.out.println("!!!!");
+        System.out.println(STORAGE_LOCATION);
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement(MESSAGES);
+        doc.appendChild(rootElement);
+
+        Transformer transformer = getTransformer();
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(STORAGE_LOCATION));
+        transformer.transform(source, result);
+    }
+
+    public static synchronized void addData(Message message) throws ParserConfigurationException, SAXException, IOException, TransformerException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(STORAGE_LOCATION);
+        document.getDocumentElement().normalize();
+
+        Element root = document.getDocumentElement();
+
+        Element messageElement = document.createElement(MESSAGE);
+        root.appendChild(messageElement);
+
+        messageElement.setAttribute(ID, Double.toString(message.getId()));
+
+        Element user = document.createElement(USER);
+        user.appendChild(document.createTextNode(message.getUser()));
+        messageElement.appendChild(user);
+
+        Element messageText = document.createElement(MESSAGETEXT);
+        messageText.appendChild(document.createTextNode(message.getMessage()));
+        messageElement.appendChild(messageText);
+
+        DOMSource source = new DOMSource(document);
+
+        Transformer transformer = getTransformer();
+
+        StreamResult result = new StreamResult(STORAGE_LOCATION);
+        transformer.transform(source, result);
+    }
+
+    public static synchronized void updateData(Message message) throws ParserConfigurationException, SAXException, IOException, TransformerException, XPathExpressionException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(STORAGE_LOCATION);
+        document.getDocumentElement().normalize();
+        Node messageToUpdate = getNodeById(document, Double.toString(message.getId()));
+
+        if (messageToUpdate != null) {
+
+            NodeList childNodes = messageToUpdate.getChildNodes();
+
+            for (int i = 0; i < childNodes.getLength(); i++) {
+
+                Node node = childNodes.item(i);
+
+                if (MESSAGETEXT.equals(node.getNodeName())) {
+                    node.setTextContent(message.getMessage());
+                }
+            }
+
+            Transformer transformer = getTransformer();
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(STORAGE_LOCATION));
+            transformer.transform(source, result);
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public static synchronized boolean doesStorageExist() {
+        File file = new File(STORAGE_LOCATION);
+        return file.exists();
+    }
+
+    public static synchronized List<Message> getMessages() throws SAXException, IOException, ParserConfigurationException {
+        return getSubMessagesByIndex(0);
+    }
+
+    public static synchronized List<Message> getSubMessagesByIndex(int index) throws ParserConfigurationException, SAXException, IOException {
+        List<Message> messages = new ArrayList<Message>();
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(STORAGE_LOCATION);
+        document.getDocumentElement().normalize();
+        Element root = document.getDocumentElement();
+        NodeList messageList = root.getElementsByTagName(MESSAGE);
+        for (int i = index; i < messageList.getLength(); i++) {
+            Element messageElement = (Element) messageList.item(i);
+            String id = messageElement.getAttribute(ID);
+            String user = messageElement.getElementsByTagName(USER).item(0).getTextContent();
+            String message = messageElement.getElementsByTagName(MESSAGETEXT).item(0).getTextContent();
+
+            messages.add(new Message(message, user,  Double.parseDouble(id)));
+        }
+        return messages;
+    }
+
+    public static synchronized int getStorageSize() throws SAXException, IOException, ParserConfigurationException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(STORAGE_LOCATION);
+        document.getDocumentElement().normalize();
+        Element root = document.getDocumentElement();
+        return root.getElementsByTagName(MESSAGE).getLength();
+    }
+
+    private static Node getNodeById(Document doc, String id) throws XPathExpressionException {
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        XPathExpression expr = xpath.compile("//" + MESSAGE + "[@id='" + id + "']");
+        return (Node) expr.evaluate(doc, XPathConstants.NODE);
+    }
+
+    private static Transformer getTransformer() throws TransformerConfigurationException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        return transformer;
+    }
+}
